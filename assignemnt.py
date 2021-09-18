38ad010d7690f40e0c3818f2d72237edb6bb4b0e
Abhi-1298 <89782763+Abhi-1298@users.noreply.github.com>
2021-09-18 16:58:27 +0530

    Please check is there any error
    It doesn't take different number only take same one digit or two digit only

---
+++ assignemnt.py
@@ -0,0 +1,16 @@
+# 3) python program which will accept list of numbers  and sort them in both asc and desc order.
+def fun():
+    n = int(input("Enter number of elements : "))  # n=5
+    if(n<=0):
+        print("{} is invalid input:".format(n))
+    else:
+        list=[]
+        for i in range(0,n):  
+            new_list=input()
+            list.append(new_list)
+        print('After appending the list in new : {}'.format(list))
+        list.sort()     
+        print('the list in asc order : {}'.format(list)) 
+        list.sort(reverse=True)   
+        print('The list in desc order:{}'.format(list))
+fun()
